7dfb4646710de83d553725d9f2db4257150895a5
windofme1109 <wuzengyan1314@163.com>
2020-07-27 18:43:37 +0800

    learning TypeScript

---
+++ 01-helloWorld.ts
@@ -0,0 +1,8 @@
+function sayHello(person: String) {
+    return `hello, ${person}` ;
+}
+
+let user = 'Tom' ;
+//user为数组类型，编译时会报错：error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'String'.
+// let user = [1, 2, 3] ;
+console.log(sayHello(user)) ;
